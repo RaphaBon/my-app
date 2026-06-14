ba3179be291ad4bb5dd56c6f18bc5afaa13649b6
RaphaBack <raphaelalvaresbonjardim@gmail.com>
2026-06-14 16:59:12 -0300

    First And Unic Try

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'postgres_db';
+$port = '5432';
+$dbname = 'educode_db';
+$user = 'postgres';
+$pass = 'senha_da_nasa';
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "✅ Conectado ao PostgreSQL com sucesso!";
+} catch (PDOException $e) {
+    die("❌ Erro na conexão: " . $e->getMessage());
+}
